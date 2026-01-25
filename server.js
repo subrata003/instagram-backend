2e810e69393c06552326697124806ded48856cc1
subrata003 <subratajpg2003@gmail.com>
2026-01-25 10:13:06 +0530

    backend change

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
-"this is my app"
-"subrata"
+import express from 'express';
+const app=express();
+const PORT=3000;
+
+
+
+app.get('/',(req,res)=>{
+ res.send('Hello World');
+})
+
+app.listen(PORT,()=>{
+ console.log(`Server is running on http://localhost:${PORT}`);
+})
